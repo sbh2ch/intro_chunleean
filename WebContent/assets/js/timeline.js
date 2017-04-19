884d4fc278a4dac896840edb6476493439dd2c39
kiost-PC\kiost <sbh2ch@gmail.com>
2017-04-19 10:55:14 +0900

    update 0419

--- WebContent/assets/js/timeline.js
+++ WebContent/assets/js/timeline.js
@@ -38,48 +38,9 @@ function triggerReveals() {
 			x : 90
 		}
 	})
-
 	sr.reveal('.scaleReveal', {
 		origin : 'top',
 		scale : 0.6
 	});
 }
-$(document).ready(function() {
-	console.log('okay');
-	sr.reveal('.bottomReveal', {
-		origin : 'bottom'
-	}).reveal('.leftReveal', {
-		origin : 'left'
-	}).reveal('.rightReveal', {
-		origin : 'right'
-	}).reveal('.topReveal', {
-		origin : 'top'
-	});
-
-	sr.reveal('.rotateBottomReveal', {
-		origin : 'bottom',
-		rotate : {
-			x : 90
-		}
-	}).reveal('.rotateLeftReveal', {
-		origin : 'left',
-		rotate : {
-			x : 90
-		}
-	}).reveal('.rotateRightReveal', {
-		origin : 'right',
-		rotate : {
-			x : 90
-		}
-	}).reveal('.rotateTopReveal', {
-		origin : 'top',
-		rotate : {
-			x : 90
-		}
-	})
-
-	sr.reveal('.scaleReveal', {
-		origin : 'top',
-		scale : 0.6
-	});
-});
+$(document).ready(triggerReveals());
